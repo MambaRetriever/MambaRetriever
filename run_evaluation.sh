5f4dcb90447c423e44ad09b153b35ec4a96d4f64
MambaRetriever <mambaretriever.ssm@gmail.com>
2024-10-04 18:15:18 -0700

    Update run_evaluation.sh

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -6,7 +6,7 @@ EXP_NAME='Example'
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 evaluate.py \
   --seed 0 \
-  --logit_path ... \
+  --logit_path rag_pipeline/prediction_logits \
   --model state-spaces/${MODEL} \
   --eval_data_path ... \
   --exp_name ${EXP_NAME} \
